f3552c694b4bf49011c6ce0ba2d584006a15ce1c
Athrva Panda <101466905+arnav006@users.noreply.github.com>
2024-07-25 20:02:42 +0530

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 // import About from "./components/About";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 
 function App(props) {
+  const[Mode,setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(Mode === 'light'){
+      setMode ('dark');
+      document.body.style.backgroundColor = '#032742';
+    }
+    else{
+      setMode ('light');
+      document.body.style.backgroundColor = 'light';
+    }
+    
+  }
   return (
    <>
-     <Navbar title ="textUtils "/>
+     <Navbar title ="textUtils " mode = {Mode} toggleMode = {toggleMode}/>
      
      <div className="container my-3" >
-     <TextForm heading = "Enter The Text Analyze"/>
+     <TextForm heading = "Enter The Text Analyze" mode = {Mode}/>
     {/* <About/> */}
      </div>
    </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,43 +3,49 @@ import React, {useState} from 'react'
 export default function TextForm(props){
  
    const handleUpClick = ()=>{
-// console.log("uppercase was clicked" + text);
 let newtext = text.toUpperCase()
 setText (newtext)
   }
+  
+  const handleCopyClick = ()=>{
+    var text=document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    
+      }
+
   const handleClearClick = ()=>{
-    // console.log("uppercase was clicked" + text);
+
     let newtext = '';
     setText (newtext)
       }
 
   const handleLoClick = ()=>{
-    // console.log("uppercase was clicked" + text);
     let newtext = text.toLowerCase()
     setText (newtext)
       }
   const handleOnChange = (event)=>{
-    // console.log("On Change");
     setText (event.target.value);
       }
 const [text,setText] = useState('Enter text here')
   return (
     <>
-    <div className='container'>
+    <div className='container' style = {{Color: props.mode === 'Dark'? 'white': '#032742'}}>
           <h1>{props.heading}</h1>
           <div className="mb-3">
-          <textarea className="form-Control"value={text} onChange={handleOnChange} id ="myBox"rows="8" ></textarea>
+          <textarea className="form-Control"value={text} onChange={handleOnChange} style = {{backgroundColor: props.mode === 'dark'? 'grey': 'white', Color: props.mode === 'dark'? 'white': '#032742' }} id ="myBox"rows="8" ></textarea>
           </div>
           <button className = "btn btn-primary mx-2" onClick =  {handleUpClick}>Convert to Uppercase </button>
           <button className = "btn btn-primary mx-2" onClick =  {handleLoClick}>Convert to Lowercase </button>
           <button className = "btn btn-primary mx-2" onClick =  {handleClearClick}>Clear</button>
+          <button className = "btn btn-primary mx-2" onClick =  {handleCopyClick}>Copy</button>
     </div>
-    <div className="container" my-3>
+    <div className="container" my-3 style = {{Color: props.mode === 'dark'? 'white': '#032742'}}>
       <h1>Your Text Summary</h1>
       <p>{text.split(" ").length} words and {text.length} Character</p>
       <p>{0.008*text.split(" ").length} Minutes Read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter Something In Textbox Above To Preview It Here"}</p>
     </div>
     </>
   )

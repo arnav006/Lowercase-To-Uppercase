dab2c349bf5f72e9ec9c48ec02302a3d4acf033f
Athrva Panda <101466905+arnav006@users.noreply.github.com>
2024-07-25 20:32:07 +0530

    commit 4

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,18 +30,18 @@ setText (newtext)
 const [text,setText] = useState('Enter text here')
   return (
     <>
-    <div className='container' style = {{Color: props.mode === 'Dark'? 'white': '#032742'}}>
+    <div className='container' style = {{color: props.mode === 'dark'? 'white': '#032742'}}>
           <h1>{props.heading}</h1>
           <div className="mb-3">
-          <textarea className="form-Control"value={text} onChange={handleOnChange} style = {{backgroundColor: props.mode === 'dark'? 'grey': 'white', Color: props.mode === 'dark'? 'white': '#032742' }} id ="myBox"rows="8" ></textarea>
+          <textarea className="form-Control"value={text} onChange={handleOnChange} style={{backgroundColor: props.mode=== 'dark'? 'grey': 'white', color: props.mode === 'dark'? 'white': '#032742' }} id ="myBox"rows="8" ></textarea>
           </div>
           <button className = "btn btn-primary mx-2" onClick =  {handleUpClick}>Convert to Uppercase </button>
           <button className = "btn btn-primary mx-2" onClick =  {handleLoClick}>Convert to Lowercase </button>
           <button className = "btn btn-primary mx-2" onClick =  {handleClearClick}>Clear</button>
           <button className = "btn btn-primary mx-2" onClick =  {handleCopyClick}>Copy</button>
     </div>
-    <div className="container" my-3 style = {{Color: props.mode === 'dark'? 'white': '#032742'}}>
-      <h1>Your Text Summary</h1>
+    <div className="container" my-3 style = {{color: props.mode === 'dark'? 'white': '#032742'}}>
+      <h2>Your Text Summary</h2>
       <p>{text.split(" ").length} words and {text.length} Character</p>
       <p>{0.008*text.split(" ").length} Minutes Read</p>
       <h2>Preview</h2>
